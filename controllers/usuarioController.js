60fc52ac80ccbc6d86ef7d09a8303b6df4590908
Jose C. Santos <zderimanz@hotmail.com>
2024-11-07 19:35:52 +0100

    Manejamos la validación del formulario con express-validator

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,6 @@
+import { check, validationResult } from 'express-validator'
 import User from '../models/User.js'
+
 const form_login = (req, res) => {
     res.render('auth/form-login', {
          pagina: "Iniciar sesión"
@@ -11,8 +13,18 @@ const form_register = (req, res) => {
     })
 }
 const register = async (req, res) => {
-    const user = await User.create(req.body)
-    res.json({user})     
+
+    await check('name')
+        .notEmpty().withMessage('El nombre es un campo requerido').run(req)
+    await check('email')
+        .isEmail().withMessage('Email no válido').run(req)
+    await check('password')
+        .isLength({ min: 6}).withMessage('El password debe contener al menos 6 caracteres').run(req)
+    await check('repetir-password')
+        .equals('password').withMessage('Los passwords deben ser iguales').run(req)
+    let validation = validationResult(req)
+    //const user = await User.create(req.body)
+    res.json(validation.array())     
 }
 const forgotPassword = (req, res) => {
     res.render('auth/forgot-password', {
