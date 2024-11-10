1517426ebe8ccb3c4eea0e50f098e0fc1ac62390
Jose C. Santos <zderimanz@hotmail.com>
2024-11-10 15:42:25 +0100

    En el controlador de la ruta crear propiedad consultamos a la BD las categorias y precios para poblar los select

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,3 +1,6 @@
+import Category from '../models/Category.js'
+import Price from '../models/Price.js'
+
 
 const adminPanel = (req, res) => {
     res.render('properties/admin-panel', {
@@ -6,10 +9,17 @@ const adminPanel = (req, res) => {
     })
 }
 
-const create = (req, res) => {
+const create = async (req, res) => {
+
+    const [categories, prices] = await Promise.all([
+        Category.findAll(),
+        Price.findAll()
+    ])
     res.render('properties/create', {
         pagina: 'Crear una propiedad',
-        header: true
+        header: true,
+        categories,
+        prices
     })
 }
 
