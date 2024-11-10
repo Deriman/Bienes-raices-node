38ca7ea57d44660b9b15c11f2cc95c2834d40e6f
Jose C. Santos <zderimanz@hotmail.com>
2024-11-10 11:56:25 +0100

    Obtenemos la lat y lng del pin mediante un evento del marker.getLanLng()

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,17 +1,25 @@
 (function() {
-    const lat = 42.6713956;
-    const lng = -2.0578033;
-    const mapa = L.map('mapa').setView([lat, lng ], 13);
+    const lat = 42.671581927092234;
+    const lng = -2.028511762619019;
+    const mapa = L.map('mapa').setView([lat, lng ], 16);
     let marker;
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
 
-    // Pin en el mapa (se puede mover, y se centra sobre él)
+    // Pin en el mapa (se puede mover)
     marker = new L.marker([lat, lng], {
         draggable: true,
         autoPan: true
     }).addTo(mapa)
+
+    //Detectar movimiento del pin
+    marker.on('moveend', function(e){
+        marker = e.target;
+        const position = marker.getLatLng();
+        console.log({position})
+        mapa.panTo(new L.LatLng(position.lat, position.lng))
+    })
 
 })()
