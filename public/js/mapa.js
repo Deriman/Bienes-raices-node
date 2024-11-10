7f762f30d8f0c56a13db725f9ac045ce2da5c1d6
Jose C. Santos <zderimanz@hotmail.com>
2024-11-10 11:23:13 +0100

    Ponemos un pin en el mapa con la ubicación (lat, lng) con draggable a true y autoPan true para poder moverlo y enfocarlo

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -16,7 +16,7 @@
   \************************/
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n(function() {\r\n    const lat = 42.6713956;\r\n    const lng = -2.0578033;\r\n    const mapa = L.map('mapa').setView([lat, lng ], 13);\r\n\r\n    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\r\n        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\r\n    }).addTo(mapa);\r\n\r\n\r\n})()\n\n//# sourceURL=webpack://bienes-raices/./src/js/mapa.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n(function() {\r\n    const lat = 42.6713956;\r\n    const lng = -2.0578033;\r\n    const mapa = L.map('mapa').setView([lat, lng ], 13);\r\n    let marker;\r\n\r\n    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\r\n        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\r\n    }).addTo(mapa);\r\n\r\n    // Pin en el mapa (se puede mover, y se centra sobre él)\r\n    marker = new L.marker([lat, lng], {\r\n        draggable: true,\r\n        autoPan: true\r\n    }).addTo(mapa)\r\n\r\n})()\n\n//# sourceURL=webpack://bienes-raices/./src/js/mapa.js?");
 
 /***/ })
 
--- src/js/mapa.js
+++ src/js/mapa.js
@@ -2,10 +2,16 @@
     const lat = 42.6713956;
     const lng = -2.0578033;
     const mapa = L.map('mapa').setView([lat, lng ], 13);
+    let marker;
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
 
+    // Pin en el mapa (se puede mover, y se centra sobre él)
+    marker = new L.marker([lat, lng], {
+        draggable: true,
+        autoPan: true
+    }).addTo(mapa)
 
 })()
