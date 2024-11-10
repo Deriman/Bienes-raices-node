af271eb2f4865d73a34a915fb1f1f0bf12e23408
Jose C. Santos <zderimanz@hotmail.com>
2024-11-10 12:16:03 +0100

    Obtenemos información de la posición del pin (calle, número, codigo postal...) a traves de utilizar Provider y Geocoder.

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -3,6 +3,8 @@
     const lng = -2.028511762619019;
     const mapa = L.map('mapa').setView([lat, lng ], 16);
     let marker;
+    // Utilizar Provider y Geocoder
+    const geocodeService = L.esri.Geocoding.geocodeService();
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -18,8 +20,13 @@
     marker.on('moveend', function(e){
         marker = e.target;
         const position = marker.getLatLng();
-        console.log({position})
+        console.log(position)
         mapa.panTo(new L.LatLng(position.lat, position.lng))
+
+        // Obtener la información de las calles al soltar el pin
+        geocodeService.reverse().latlng(position, 16).run(function(error, result){
+            marker.bindPopup(result.address.LongLabel)
+        })
     })
 
 })()
