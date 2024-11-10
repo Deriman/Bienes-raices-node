a3615314ea1eed1a9ab5b6b35c8d875f9c460816
Jose C. Santos <zderimanz@hotmail.com>
2024-11-10 11:15:45 +0100

    Mostramos el mapa, para ello incluimos un js en el archivo mapa.js y un css en la pagina create.pug

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -16,7 +16,7 @@
   \************************/
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\nalert('Soy el mapa')\n\n//# sourceURL=webpack://bienes-raices/./src/js/mapa.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n(function() {\r\n    const lat = 42.6713956;\r\n    const lng = -2.0578033;\r\n    const mapa = L.map('mapa').setView([lat, lng ], 13);\r\n\r\n    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\r\n        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\r\n    }).addTo(mapa);\r\n\r\n\r\n})()\n\n//# sourceURL=webpack://bienes-raices/./src/js/mapa.js?");
 
 /***/ })
 
--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,11 @@
-alert('Soy el mapa')
+(function() {
+    const lat = 42.6713956;
+    const lng = -2.0578033;
+    const mapa = L.map('mapa').setView([lat, lng ], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+})()
